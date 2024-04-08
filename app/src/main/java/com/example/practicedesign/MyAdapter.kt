8b3f44f695c752e54e4de90be4713e9f9b13cb95
Tahsin007 <tahsinahmed.iit@gmail.com>
2024-04-09 03:42:04 +0600

    Commit With recycler view

---
+++ app/src/main/java/com/example/practicedesign/MyAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.practicedesign
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import de.hdodenhof.circleimageview.CircleImageView
+
+class MyAdapter(
+    var Images: ArrayList<Int>,
+    var details: ArrayList<String>,
+    var Names: ArrayList<String>,
+    var context: Context
+): RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+    class ViewHolder(ItemView:View):RecyclerView.ViewHolder(ItemView) {
+        var textViewName:TextView = ItemView.findViewById(R.id.cardName)
+        var textViewDescription:TextView = ItemView.findViewById(R.id.cardDescriptions)
+        var cardImage:CircleImageView =ItemView.findViewById(R.id.cardImage)
+
+
+    }
+//To create new view instance when layout manager failed to load any view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView =LayoutInflater.from(parent.context).inflate(R.layout.card_design,parent,false)
+        return ViewHolder(itemView)
+    }
+//how many list items are present in your array
+    override fun getItemCount(): Int {
+        return Names.size
+    }
+//it is to use fill the views with datas
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textViewName.text = Names.get(position)
+        holder.textViewDescription.text = details.get(position)
+        holder.cardImage.setImageResource(Images.get(position))
+    }
+
+
+}
--- app/src/main/java/com/example/practicedesign/RecyclerViews.kt
+++ app/src/main/java/com/example/practicedesign/RecyclerViews.kt
@@ -1,11 +1,46 @@
 package com.example.practicedesign
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Call.Details
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViews : AppCompatActivity() {
+    lateinit var recycleV:RecyclerView
+    var Images = ArrayList<Int>()
+    var details = ArrayList<String>()
+    var Names = ArrayList<String>()
+
+    lateinit var adapter:MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_views)
+
+        recycleV = findViewById(R.id.recycleList)
+        recycleV.layoutManager = LinearLayoutManager(this@RecyclerViews)
+
+        Names.add("Tahsin Ahmed")
+        Names.add("Wakil Ahmed")
+        Names.add("Mamun Ahmed")
+        Names.add("Tanvir Ahmed")
+        Names.add("Junaed Ahmed")
+
+        details.add("This Picture is belonged to Tahsin Ahmed")
+        details.add("This picture is belonged to Wakil Ahmed")
+        details.add("This picture is belonged to Mamun Ahmed")
+        details.add("This picture is belonged to Tanvir Ahmed")
+        details.add("This picture is belonged to Junaed Ahmed")
+
+        Images.add(R.drawable.formalimage1)
+        Images.add(R.drawable.formalimage2)
+        Images.add(R.drawable.formalimage1)
+        Images.add(R.drawable.formalimage2)
+        Images.add(R.drawable.formalimage1)
+
+        adapter = MyAdapter(Images,details,Names,this@RecyclerViews)
+        recycleV.adapter =adapter
     }
 }
